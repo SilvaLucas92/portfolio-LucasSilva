3b01d0edeb77568a5939ddc1a06c004e91df85dd
SilvaLucas92 <l.silva8692@gmail.com>
2022-04-11 22:47:03 -0300

    cambios en media

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,14 @@
 window.addEventListener('load', () => {
     const toggleBtn = document.getElementById('btn-hamburguer');
     const ulNav = document.querySelector('.ul-nav');
+    
+    const aNav = document.querySelectorAll('.nav-a')
     toggleBtn.addEventListener('click', () => {
         ulNav.classList.toggle('active');
-    })
+    });
+    aNav.forEach(btn => {
+        btn.addEventListener('click', () => {
+            ulNav.classList.remove('active');
+        })
+    }); 
 })
